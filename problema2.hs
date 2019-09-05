66fd46df19573084e908d2a519d12280b461c64d
Teo <teogallarza@gmail.com>
2019-09-05 08:10:02 -0300

    second done

--- problema2.hs
+++ problema2.hs
@@ -1,7 +1,10 @@
 fritz :: Int -> Int
-fritz n | (n == 0) = 0
-        | (n == 1) = 1
-        | otherwise = (fritz (n-1) ) /2 + 3 * (fritz (n-2)
+fritz n 
+    | (n == 0) = 0
+    | (n == 1) = 1
+    | otherwise = (quot (fritz(n-1)) 2) + 3 * (fritz (n-2))
 
 main = do
-    putStrLn (show(fritz 5))
+    n <- getLine
+    let n1 = (read n :: Int)
+    putStrLn (show(fritz n1))
